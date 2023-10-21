ee868cab1c5a05d891e9a1b322a04cebbc293bae
Zaid <zzaid247@gmail.com>
2023-10-21 14:19:17 +0200

    First commt

---
+++ src/main/java/com/marktplatz/marktplatz/BackendMarktplatzApplication.java
@@ -0,0 +1,13 @@
+package com.marktplatz.marktplatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMarktplatzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendMarktplatzApplication.class, args);
+    }
+
+}
