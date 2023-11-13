95f7357da919e594980cc401579979fa7057a0cc
Kowalski187 <olivernix31@gmail.com>
2023-11-13 18:27:32 +0100

    Create Product

--- src/app/createproduct/createproduct.component.ts
+++ src/app/createproduct/createproduct.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
 import { RegisterDialogComponent } from '../register-dialog/register-dialog.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-createproduct',
@@ -10,8 +11,15 @@ import { RegisterDialogComponent } from '../register-dialog/register-dialog.comp
 })
 export class CreateproductComponent {
   account: boolean = false;
+  productform: FormGroup;
 
-  constructor(private dialog:MatDialog) {}
+  constructor(private dialog:MatDialog, private formBuilder: FormBuilder) {
+    this.productform = this.formBuilder.group({
+      product: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+    })
+  }
 
   openDialogLogin() {
     this.dialog.open(LoginDialogComponent)
@@ -19,5 +27,9 @@ export class CreateproductComponent {
 
   openDialogRegister() {
     this.dialog.open(RegisterDialogComponent)
+  }
+
+  closeDialog() {
+    this.dialog.closeAll()
   }
 }
