11d299da78182a474427440042b62c968d742774
Kowalski187 <olivernix31@gmail.com>
2023-11-16 14:58:32 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, Inject, Input, OnInit, OnDestroy } from '@angular/core';
 import { SidebarstatusService } from './sidebarstatus.service';
-import { MediaObserver, MediaChange} from '@angular/flex-layout';
+import { MediaObserver, MediaChange } from '@angular/flex-layout';
 import { Subscription} from 'rxjs';
+
 
 
 @Component({
@@ -9,20 +10,31 @@ import { Subscription} from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  messagesObserver = this.sidebarServ.messagesObserver;
-  productObserver = this.sidebarServ.productObserver;
-  accountObserver = this.sidebarServ.accountObserver;
-  hompageObserver = this.sidebarServ.hompageObserver;
+  export class AppComponent implements OnInit, OnDestroy {
+    messagesObserver = this.sidebarServ.messagesObserver;
+    productObserver = this.sidebarServ.productObserver;
+    accountObserver = this.sidebarServ.accountObserver;
+    hompageObserver = this.sidebarServ.hompageObserver;
+    username = ['Oliver','Zaid','Besmir ']
+    postProduct = ['Fernseher', 'Fernseher', 'Fernseher']
+    postPrice = ['300€', '290€', '320€']
+    title = 'EbayTest';
 
-  constructor (private sidebarServ: SidebarstatusService, public mediaObserver:MediaObserver) {}
+    mediaSub:Subscription;
+
+    constructor(private sidebarServ: SidebarstatusService, public mediaObserver: MediaObserver) {
+      console.log('MediaObserver:', mediaObserver);
+      this.mediaSub = new Subscription();
+    }
 
 
+    ngOnInit(): void {
+      this.mediaSub = this.mediaObserver.media$.subscribe((result:MediaChange) =>{
+        console.log(result.mqAlias)
+      })
+    }
 
-  username = ['Oliver','Zaid','Besmir ']
-  postProduct = ['Fernseher', 'Fernseher', 'Fernseher']
-  postPrice = ['300€', '290€', '320€']
-  title = 'EbayTest';
-
-
-}
+    ngOnDestroy(): void {
+      this.mediaSub.unsubscribe();
+    }
+  }
