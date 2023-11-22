f00e3af4a633fbccafb525e43e5c3bb31f49f1a8
Kowalski187 <olivernix31@gmail.com>
2023-11-22 19:39:06 +0100

    Bug fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { AppComponent } from './app.component';
 import { ProdukteComponent } from './produkte/produkte.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { LeftsidebarComponent } from './leftsidebar/leftsidebar.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NgIf, NgFor } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
@@ -36,8 +34,6 @@ import { ToastrModule } from 'ngx-toastr';
   declarations: [
     AppComponent,
     ProdukteComponent,
-    LeftsidebarComponent,
-    FooterComponent,
     LoginDialogComponent,
     RegisterDialogComponent,
     CreateproductComponent,
